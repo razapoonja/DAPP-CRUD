f8eb7cdf9cdda6f0fb02cf0d1c476e97e2c0edea
Raza Poonja <razapoonja@gmail.com>
2020-07-29 05:52:36 +0500

    completed Crud.sol

--- contracts/Crud.sol
+++ contracts/Crud.sol
@@ -16,4 +16,24 @@ contract Crud {
 
 		nextId++;
 	}
+
+	function read(uint id) public view returns(uint, string memory) {
+		for (uint i = 0; i < users.length; i++) {
+			if (users[i].id == id) {
+				return (users[i].id, users[i].name);
+			}
+		}
+	}
+
+	function update(uint id, string memory name) public {
+		for (uint i = 0; i < users.length; i++) {
+			if (users[i].id == id) {
+				users[i].name = name;
+			}
+		}
+	}
+
+	function destroy(uint id) public {
+		delete users[id];
+	}
 }
